1590fa0d51d82fb3d004932da420e41a1651c98a
chris <cctitan53@gmail.com>
2022-12-21 18:41:07 +0100

    ~fix variable aus vorigem commit falsch benannt

--- index.mjs
+++ index.mjs
@@ -6,7 +6,7 @@ const app = express();
 const port = parseInt(process.argv[2]) ? parseInt(process.argv[2]) : '8080';
 const staticPath = path.join(path.dirname(process.argv[1]), 'client/dist/'); // Pfad aendern falls /index.mjs zu /server/expressBasis.js wird -> '../client/dist/'
 
-app.use(express.static(distPath));
+app.use(express.static(staticPath));
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
